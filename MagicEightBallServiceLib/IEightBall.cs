47a33590d89758e45d02d4f007ab1ff4a88aafd9
LinkinWu <wulinjian@188.com>
2013-12-18 14:03:15 +0800

    加了真的很强大

--- MagicEightBallServiceLib/IEightBall.cs
+++ MagicEightBallServiceLib/IEightBall.cs
@@ -5,6 +5,7 @@ using System.ServiceModel;
 using System.Text;
 //added in github website
 //wljalajdfdfddddddddd
+//wlj:真的很强大
 namespace MagicEightBallServiceLib
 {   [ServiceContract]
     interface IEightBall
